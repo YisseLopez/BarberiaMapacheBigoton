55b260b01164ec7701f528b0cd2c12d11a4554f5
Lesly Yissel <leslyissel@gmail.com>
2024-09-26 15:01:56 -0600

    actualizacion

--- MapacheBigoton/FrmCitasRegistradas.Designer.cs
+++ MapacheBigoton/FrmCitasRegistradas.Designer.cs
@@ -28,12 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmCitasRegistradas";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmCitasRegistradas));
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            dgvCitasAgendadas = new DataGridView();
+            btnSalir = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvCitasAgendadas).BeginInit();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Ravie", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(205, 61);
+            label2.Name = "label2";
+            label2.Size = new Size(377, 43);
+            label2.TabIndex = 7;
+            label2.Text = "Citas Agendadas";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(59, 23);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(117, 112);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 16;
+            pictureBox1.TabStop = false;
+            // 
+            // dgvCitasAgendadas
+            // 
+            dgvCitasAgendadas.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvCitasAgendadas.Location = new Point(103, 158);
+            dgvCitasAgendadas.Name = "dgvCitasAgendadas";
+            dgvCitasAgendadas.Size = new Size(591, 194);
+            dgvCitasAgendadas.TabIndex = 17;
+            // 
+            // btnSalir
+            // 
+            btnSalir.Font = new Font("Lucida Handwriting", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSalir.Location = new Point(525, 374);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(157, 48);
+            btnSalir.TabIndex = 25;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            btnSalir.Click += btnSalir_Click;
+            // 
+            // FrmCitasRegistradas
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSalir);
+            Controls.Add(dgvCitasAgendadas);
+            Controls.Add(pictureBox1);
+            Controls.Add(label2);
+            Name = "FrmCitasRegistradas";
+            Text = "FrmCitasRegistradas";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvCitasAgendadas).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label2;
+        private PictureBox pictureBox1;
+        private DataGridView dgvCitasAgendadas;
+        private Button btnSalir;
     }
 }
--- MapacheBigoton/FrmCitasRegistradas.cs
+++ MapacheBigoton/FrmCitasRegistradas.cs
@@ -15,6 +15,29 @@ namespace MapacheBigoton
         public FrmCitasRegistradas()
         {
             InitializeComponent();
+            dgvCitasAgendadas.ColumnCount = 6;
+            dgvCitasAgendadas.Columns[0].Name = "Nombre del cliente";
+            dgvCitasAgendadas.Columns[1].Name = "Telefono";
+            dgvCitasAgendadas.Columns[2].Name = "Servicio solicitado";
+            dgvCitasAgendadas.Columns[3].Name = "Costo del servicio";
+            dgvCitasAgendadas.Columns[4].Name = "Hora del servicio";
+            dgvCitasAgendadas.Columns[5].Name = "Fecha del servicio";
+            dgvCitasAgendadas.Columns[6].Name = "Barbero que realizará el servicio";
+        }
+
+        public void AgregarCita(FrmAgregarCita.Cita cita)
+        {
+            string[] row = {
+                cita.NombreCliente,
+                cita.Telefono,
+                cita.ServicioSolicitado,
+                cita.Hora
+            };
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
